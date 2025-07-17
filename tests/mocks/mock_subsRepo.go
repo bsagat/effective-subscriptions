f39b4449ac61abd77875c2a839479f46c3c932f3
bsagat <sagatbekbolat854@gmail.com>
2025-07-17 19:11:39 +0500

    chore: add tests

---
+++ tests/mocks/mock_subsRepo.go
@@ -0,0 +1,64 @@
+package mock
+
+import (
+	"context"
+	"submanager/internal/domain"
+	"time"
+)
+
+type MockSubsRepo struct {
+}
+
+func NewMockSubsRepo() *MockSubsRepo {
+	return &MockSubsRepo{}
+}
+
+func (repo *MockSubsRepo) Create(ctx context.Context, subs domain.Subscription) error {
+	return nil
+}
+func (repo *MockSubsRepo) Delete(ctx context.Context, serviceName string, userID string) error {
+	if serviceName == "notexist" {
+		return domain.ErrSubsNotFound
+	}
+	return nil
+}
+func (repo *MockSubsRepo) DeleteList(ctx context.Context, userID string) error {
+	if userID == "notexist" {
+		return domain.ErrSubsNotFound
+	}
+	return nil
+}
+func (repo *MockSubsRepo) Get(ctx context.Context, serviceName string, userID string) (domain.Subscription, error) {
+	if serviceName == "notexist" {
+		return domain.Subscription{}, domain.ErrSubsNotFound
+	}
+	return domain.Subscription{}, nil
+}
+func (repo *MockSubsRepo) IsUnique(ctx context.Context, serviceName string, userID string) (bool, error) {
+	if serviceName == "notunique" {
+		return false, nil
+	}
+	return true, nil
+}
+func (repo *MockSubsRepo) List(ctx context.Context, userID string) ([]domain.Subscription, error) {
+	if userID == "notexist" {
+		return []domain.Subscription{}, nil
+	}
+	return []domain.Subscription{
+		{},
+	}, nil
+}
+func (repo *MockSubsRepo) SubsListByFilter(ctx context.Context, start time.Time, end time.Time, serviceName string, userID string) ([]domain.Subscription, error) {
+	if serviceName == "notexist" {
+		return []domain.Subscription{}, nil
+	}
+	return []domain.Subscription{
+		{},
+	}, nil
+}
+func (repo *MockSubsRepo) Update(ctx context.Context, subs domain.Subscription) error {
+	if subs.ServiceName == "notexist" {
+		return domain.ErrSubsNotFound
+	}
+	return nil
+}
---
+++ tests/service_test.go
@@ -0,0 +1,158 @@
+package tests
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"submanager/internal/domain"
+	"submanager/internal/service"
+	mock "submanager/tests/mocks"
+	"testing"
+	"time"
+)
+
+var (
+	serv *service.SubsService
+)
+
+func TestMain(m *testing.M) {
+	slog.Info("Starting the tests...")
+
+	// Initialize the mock repository and service
+	repo := mock.NewMockSubsRepo()
+	log := slog.Default()
+	serv = service.NewSubsService(repo, log)
+
+	defer os.Exit(m.Run())
+	slog.Info("Test has been finished...")
+}
+
+func TestCreateSubscription(t *testing.T) {
+	ctx := context.Background()
+
+	// Define a sample subscription
+	sampleSubscription := domain.Subscription{
+		ServiceName: "TestService",
+		UserID:      "user123",
+		StartDate:   time.Now(),
+		Price:       100,
+	}
+
+	// Default test case
+	err := serv.CreateSubscription(ctx, sampleSubscription)
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+
+	// Check if subscription not unique
+	sampleSubscription.ServiceName = "notunique"
+	if err := serv.CreateSubscription(ctx, sampleSubscription); err == nil {
+		t.Errorf("Expected error %v, got %v", domain.ErrSubNotUnique, err)
+	}
+}
+
+func TestGetSubscription(t *testing.T) {
+	ctx := context.Background()
+
+	serviceName, userID := "TestService", "user123"
+
+	// Default test case
+	_, err := serv.GetSubscription(ctx, serviceName, userID)
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+
+	// Check if subscription not found
+	serviceName = "notexist"
+	_, err = serv.GetSubscription(ctx, serviceName, userID)
+	if err == nil {
+		t.Errorf("Expected error %v, got %v", domain.ErrSubsNotFound, err)
+	}
+}
+
+func TestGetSubscriptionList(t *testing.T) {
+	ctx := context.Background()
+	userId := "user123"
+
+	// Default test case
+	if _, err := serv.GetSubscriptionList(ctx, userId); err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+
+	// Check if subscription list not found
+	userId = "notexist"
+	if _, err := serv.GetSubscriptionList(ctx, userId); err == nil {
+		t.Errorf("Expected error %v, got %v", domain.ErrSubsNotFound, err)
+	}
+}
+
+func TestUpdateSubscription(t *testing.T) {
+	ctx := context.Background()
+
+	// Define a sample subscription
+	sampleSubscription := domain.Subscription{
+		ServiceName: "TestService",
+		UserID:      "user123",
+		StartDate:   time.Now(),
+		Price:       100,
+	}
+
+	// Default test case
+	if err := serv.UpdateSubscription(ctx, sampleSubscription); err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+
+	// Check if subscription not found
+	sampleSubscription.ServiceName = "notexist"
+	if err := serv.UpdateSubscription(ctx, sampleSubscription); err == nil {
+		t.Errorf("Expected error %v, got %v", domain.ErrSubsNotFound, err)
+	}
+}
+
+func TestDeleteSubscription(t *testing.T) {
+	ctx := context.Background()
+
+	// Default test case
+	serviceName, userID := "TestService", "user123"
+	if err := serv.DeleteSubscription(ctx, serviceName, userID); err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+
+	// Check if subscription not found
+	serviceName = "notexist"
+	if err := serv.DeleteSubscription(ctx, serviceName, userID); err == nil {
+		t.Errorf("Expected error %v, got %v", domain.ErrSubsNotFound, err)
+	}
+}
+
+func TestDeleteSubscriptionList(t *testing.T) {
+	ctx := context.Background()
+
+	// Default test case
+	userID := "user123"
+	if err := serv.DeleteSubscriptionList(ctx, userID); err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+
+	// Check if subscription not found
+	userID = "notexist"
+	if err := serv.DeleteSubscriptionList(ctx, userID); err == nil {
+		t.Errorf("Expected error %v, got %v", domain.ErrSubsNotFound, err)
+	}
+}
+
+func TestGetSummaryByFilter(t *testing.T) {
+	ctx := context.Background()
+
+	// Default test case
+	start, end, serviceName, userID := time.Now(), time.Now(), "TestService", "user123"
+	if _, err := serv.GetSummaryByFilter(ctx, start, end, serviceName, userID); err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+
+	// Check if subscription not found
+	serviceName = "notexist"
+	if _, err := serv.GetSummaryByFilter(ctx, start, end, serviceName, userID); err == nil {
+		t.Errorf("Expected error %v, got %v", domain.ErrSubsNotFound, err)
+	}
+}
